48fe04d68184a25b60b067a91a6de312761faa3c
DESKTOP-9C13CL2\Asus <huynhtttrinh1982@gmail.com>
2021-10-25 21:14:09 +0700

    first commit

--- README.md
+++ README.md
@@ -6,22 +6,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## Before clicking Execute Script button
+Right-click any page element and select Inspect Element. Click the Element tab and the Network tab. You will find text of script tag (console.log('console Trinh !'); alert("Trinh");) executed and src of script tag (bootstrap.bundle.min.js) loaded
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Directive, ElementRef, OnInit, Pipe, PipeTransform, Renderer2, VERSION, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
+@Pipe({ name: 'safeHtml' })
+export class SafeHtmlPipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) { }
+
+  transform(html: any) {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
+}
+@Directive({ selector: '[runScripts]' })
+export class RunScriptsDirective implements OnInit, AfterViewInit {
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+  ngOnInit(): void {
+    // setTimeout(() => { // wait for DOM rendering
+    //   this.reinsertScripts();
+    // });
+  }
+  ngAfterViewInit() {
+    // debugger
+    // wait for DOM rendering
+    this.reinsertScripts();
+  }
+  addJsToElement(src: string, text: string = ''): HTMLScriptElement {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    if (src) {
+      script.src = src;
+    } else {
+      script.text = text;
+    }
+    this.renderer.appendChild(document.body, script);
+    return script;
+  }
+  reinsertScripts(): void {
+    debugger
+    const scripts = <HTMLScriptElement[]>this.elementRef.nativeElement.getElementsByTagName('script');
+    for (let script of scripts) {
+      if (script.src) {
+        this.addJsToElement(script.src);
+      } else
+      eval(script.text);
+    }
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'threatmetrix';
+  name = 'Angular ' + VERSION.major;
+  // html: string = `<b>script</b> <script type="text/javascript">
+  //   console.log('console Trinh !');
+  //   alert("Trinh");
+  // </script> <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">`;
+  comment: string = `<b>script</b> <script type="text/javascript">
+    console.log('console Trinh !');
+    alert("Trinh");
+  </script> <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">`;
+  @ViewChild('myDiv') divElement!: ElementRef;
+  // @ViewChild('htmlContainer') container!: ElementRef;
+  onClick() {
+    this.divElement.nativeElement.innerHTML = this.comment;
+    const scripts = <HTMLScriptElement[]>this.divElement.nativeElement.getElementsByTagName('script');
+    for (let script of scripts) {
+      if (script.src) {
+        this.addJsToElement(script.src);
+      } else
+      eval(script.text);
+    }
+  }
+  constructor(private renderer: Renderer2, private sanitizer: DomSanitizer, private elementRef: ElementRef) {
+
+  }
+
+  addJsToElement(src: string, text: string = ''): HTMLScriptElement {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    if (src) {
+      script.src = src;
+    } else {
+      script.text = text;
+    }
+    this.renderer.appendChild(document.body, script);
+    return script;
+  }
+  // ngAfterViewInit() {
+  //   // wait for DOM rendering
+  //   let scripts = this.container.nativeElement.getElementsByTagName('script');
+  //   for (let script of scripts){
+  //     eval(script.text)
+  //   }
+  // }
+
 }
